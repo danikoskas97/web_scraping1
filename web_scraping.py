a7b4618374dcf0211782fc69d9db0428a59f52df
dani koskas <koskas.dani@gmail.com>
2020-05-07 23:03:27 +0200

    adding new functionality, data saved in .csv file

--- web_scraping.py
+++ web_scraping.py
@@ -5,28 +5,22 @@ import time
 __doc__
 
 # source : http://example.webscraping.com/
-# learned in youtube video
+# learned and apply with an youtube video
 '''
 example: 
-        soup = BeautifulSoup(response.text, 'lxml') 
-        title = soup.find('title') => find the element and whats in
-        print(title) => give the <title>blablabla</title>
-        print(title.text) => give only the content of html element
-        tds = soup.findAll('td') => give all the td 
-        print(len(tds)) => give the number of td element in the html file
-        print()
-        print()
-        
-        title.findAll => 
+soup = BeautifulSoup(response.text, 'lxml') 
+title = soup.find('title') => find the element and whats in
+print(title) => give the <title>blablabla</title>
+print(title.text) => give only the content of html element
+tds = soup.findAll('td') => give all the td 
+print(len(tds)) => give the number of td element in the html file
 '''
-
-#<td>
+# <td>
 #   <div>
 #       <a href="/places/default/view/Antigua-and-Barbuda-10">
 #       <img src="/places/static/images/flags/ag.png"/>Antigua and Barbuda</a>
 #   </div>
 # </td>
-
 '''
 links = []
 
@@ -50,23 +44,21 @@ with open('urls.txt', 'w') as file:
     for link in links:
         file.write(link + "\n")
 
-
-with open('urls.txt', 'r') as file:
-    for row in file:
-        print(row)
-
 '''
 
-url = 'http://example.webscraping.com/places/default/view/Israel-108'
-
-
-
-
-
-
-
-
-
-
-
-
+'''
+# url = 'http://example.webscraping.com/places/default/view/Yemen-250'
+with open('urls.txt', 'r') as inf:
+    with open('pays.csv', 'w') as outf:
+        outf.write('pays, \t\t\t\t population\n')
+        for row in inf:
+            url = row.strip()
+            response = requests.get(url)
+            if response.ok:
+                soup = BeautifulSoup(response.text, 'lxml')
+                country = soup.find('tr', {'id': 'places_country__row'}).find('td', {'class': 'w2p_fw'})
+                pop = soup.find('tr', {'id': 'places_population__row'}).find('td', {'class': 'w2p_fw'})
+                print('Pays: ' + country.text + ' Pop: ' + pop.text)
+                outf.write(country.text + ',' + '\t' + '\t' + '\t' + pop.text.replace(',', '') + '\n')
+            time.sleep(1)
+'''
